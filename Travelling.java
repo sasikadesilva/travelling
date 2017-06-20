237dd994e65c9dabd4d5ece250b57374d1219dbc
sasika de silva <sivasasika@gmail.com>
2017-06-20 16:42:54 +0530

    Add files via upload

---
+++ Travelling.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travelling;
+
+/**
+ *
+ * @author sasika
+ */
+public class Travelling {
+        
+        String to; 
+	String from;
+	String[][] rout132= {{"100- (pettah-panadura)   ","Panadura" ,"Walana" ,"Old Galle Road" ,"Keselwatta" ,"Moratuwa" ,"Ratmalana","Mt.Lavinia", "Galkissa" ,"Dehiwala" , "Wellawatta" ,"Bambalapitiya" ,"Kollupitiya" ,"Galle Face" ,"Fort","Pettah"},
+			     {"101- (pettah-moratuwa)   ","Katubedda" ,"Ratmalana", "Mt.Lavinia","Galkissa" ,"Dehiwala", "Wellawatta" , "Bambalapitiya" ,"Kollupitiya" ,"Slave Island", "Lake House", "Fort - Pettah","moratuwa"},
+                             {"102/256- (kotahena-borupana)  ","Borupana", " Ratmalana" ,"Mt. Lavinia", "Galkissa", "Dehiwala", "Wellawatta", "Bambalapitiya", "Kollupitiya", "Galle Face", "Fort" , "Kochchikade" ,"Kotahena" },
+                             {"103- (fort-narahenpita / borella)  ","Narahenpita", "Kanatta Junction" , "Borella", "Punchi Borella",  "Maradana", "Technical College Junction", "Pettah" , "Fort"},
+                             {"104- (bambalapitiya-wattala / ja-ela)  ","Ja-ela", "Wattala", "Peliyagoda" , "New Kelani Bridge", "Orugodawatta", "Dematagoda" ,"Borella",  "Kanatta Junction" ,"Bauddhaloka Mawatha","Thummulla" ,"Bambalapitiya"},
+                             {"107- (fort-elakanda)  ","Elakanda", "Wattala", "Peliyagoda", "Old Kelani Bridge","Sirimavo Bandaranaika Road","Bloemendhal","Pettah","Fort"},
+                             {"112- (kotahena-maharagama)  ","Maharagama" ,"Navinna" ,"Delkanda" ,"Nugegoda" ,"Kirillapone" ,"Havelock Town","Dickmans Road" ,"Bambalapitiya Hindu college","Lorensz road","Bambalapitiya flats","Bambalapitiya","Dickmans Road","Kollupitiya","Galle Face","Fort","Kochchikade","Kotahena"},
+                             {"120- (pettah-piliyandala/kesbawa/horana)  ","Horana","Pokunuwita","Gonapola","Kahatuduwa","Kesbewa","Piliyandala","Boralesgamuwa","Rattanapitiya","Pepiliyana","Kohuwala","Dutugemunu Street","Pamankada","Havlock Town","Thimbirigasyaya Junction","Thummulla","Kumaratunga Munidasa Road","Cambridge Place",
+                                     "Public Library","Dharmapala Mawatha","Reid Avenue","Cinnamon Gardens","Town Hall","Ibbanwala Junction","T.B. Jayah Road","Darley Road","Gamani Hall Junction","D.r. Wijewardena Road","Lake House","Fort","Pettah"},
+                             {"108","Ja-ela", "Wattala", "Peliyagoda" , "New Kelani Bridge", "Orugodawatta", "Dematagoda" ,"Borella",  "Kanatta Junction" ,"Bauddhaloka Mawatha","Thummulla" ,"Bambalapitiya"},
+                             {"122","Ja-ela", "Wattala", "Peliyagoda" , "New Kelani Bridge", "Orugodawatta", "Dematagoda" ,"Borella",  "Kanatta Junction" ,"Bauddhaloka Mawatha","Thummulla" ,"Bambalapitiya"},
+                             {"112","Ja-ela", "Wattala", "Peliyagoda" , "New Kelani Bridge", "Orugodawatta", "Dematagoda" ,"Borella",  "Kanatta Junction" ,"Bauddhaloka Mawatha","Thummulla" ,"Bambalapitiya"},
+                             {"156","Ja-ela", "Wattala", "Peliyagoda" , "New Kelani Bridge", "Orugodawatta", "Dematagoda" ,"Borella",  "Kanatta Junction" ,"Bauddhaloka Mawatha","Thummulla" ,"Bambalapitiya"},
+                             {"187","Ja-ela", "Wattala", "Peliyagoda" , "New Kelani Bridge", "Orugodawatta", "Dematagoda" ,"Borella",  "Kanatta Junction" ,"Bauddhaloka Mawatha","Thummulla" ,"Bambalapitiya"},
+                             {"134","Ja-ela", "Wattala", "Peliyagoda" , "New Kelani Bridge", "Orugodawatta", "Dematagoda" ,"Borella",  "Kanatta Junction" ,"Bauddhaloka Mawatha","Thummulla" ,"Bambalapitiya"},
+                             {"104","Ja-ela", "Wattala", "Peliyagoda" , "New Kelani Bridge", "Orugodawatta", "Dematagoda" ,"Borella",  "Kanatta Junction" ,"Bauddhaloka Mawatha","Thummulla" ,"Bambalapitiya"},
+                             {"1045","Ja-ela", "Wattala", "Peliyagoda" , "New Kelani Bridge", "Orugodawatta", "Dematagoda" ,"Borella",  "Kanatta Junction" ,"Bauddhaloka Mawatha","Thummulla" ,"Bambalapitiya"},
+                             {"10df","Ja-ela", "Wattala", "Peliyagoda" , "New Kelani Bridge", "Orugodawatta", "Dematagoda" ,"Borella",  "Kanatta Junction" ,"Bauddhaloka Mawatha","Thummulla" ,"Bambalapitiya"},
+                             {"104","Ja-ela", "Wattala", "Peliyagoda" , "New Kelani Bridge", "Orugodawatta", "Dematagoda" ,"Borella",  "Kanatta Junction" ,"Bauddhaloka Mawatha","Thummulla" ,"Bambalapitiya"},
+                             {"177","colombo","kollupitiya","bambalapitiya","galkissa","dehiwala"},};
+    public static void main(String[] args) {
+        
+        Travelling ss = new Travelling();
+        
+        
+           
+		
+		
+	}	
+
+    
+    
+}
